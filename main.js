277711649f1ec5a2077e6ff6adb834706d0c5116
habibullousmanov <habibullousmanov82@gmail.com>
2025-11-17 16:03:27 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+"use strick"
+document.getElementById("appStoreBtn").addEventListener("click", function () {
+  window.location.href = "https://www.apple.com/app-store/";
+});
+document.getElementById("playStoreBtn").addEventListener("click", function () {
+  window.location.href = "https://play.google.com/store";
+});
+document.getElementById("appStoreBtn2").addEventListener("click", function () {
+  window.location.href = "https://www.apple.com/app-store/";
+});
+document.getElementById("playStoreBtn2").addEventListener("click", function () {
+  window.location.href = "https://play.google.com/store";
+});
